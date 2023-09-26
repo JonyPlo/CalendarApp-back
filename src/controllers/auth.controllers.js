9a56b9e618f50f20164fa284ce44b354dd1ec2cf
JonyPlo <joonyyplo@gmail.com>
2023-09-26 05:27:00 -0300

    feat(auth): :passport_control: add more config in create user controller
    Test request body in create user controller

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,13 +1,31 @@
 import { response } from 'express'; // Se importa response y se lo agrega en el parametro res para obtener el autocompletado de las propiedades de res
 
 export const loginUser = (req, res = response) => {
-  res.json({ ok: true, msg: 'login' });
+  const { email, password } = req.body;
+
+  res.json({
+    ok: true,
+    msg: 'login',
+    email,
+    password,
+  });
 };
 
 export const createUser = (req, res = response) => {
-  res.json({ ok: true, msg: 'register' });
+  const { name, email, password } = req.body;
+
+  res.json({
+    ok: true,
+    msg: 'register',
+    name,
+    email,
+    password,
+  });
 };
 
 export const renewToken = (req, res = response) => {
-  res.json({ ok: true, msg: 'renew' });
+  res.json({
+    ok: true,
+    msg: 'renew',
+  });
 };
