365c43cf2310878790e26b5d37e9af0afd04495a
JonyPlo <joonyyplo@gmail.com>
2023-09-26 08:59:45 -0300

    feat(auth): :sparkles: add auth validations
    Use express validator library to validate auth fields

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,26 +1,61 @@
 import { response } from 'express'; // Se importa response y se lo agrega en el parametro res para obtener el autocompletado de las propiedades de res
+import { validationResult } from 'express-validator';
 
 export const loginUser = (req, res = response) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  res.json({
-    ok: true,
-    msg: 'login',
-    email,
-    password,
-  });
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        ok: false,
+        errors: errors.mapped(),
+      });
+    }
+
+    res.json({
+      ok: true,
+      msg: 'login',
+      email,
+      password,
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: 'Error logging in user',
+      error,
+    });
+  }
 };
 
 export const createUser = (req, res = response) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  res.json({
-    ok: true,
-    msg: 'register',
-    name,
-    email,
-    password,
-  });
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        ok: false,
+        errors: errors.mapped(),
+      });
+    }
+
+    res.status(201).json({
+      ok: true,
+      msg: 'register',
+      name,
+      email,
+      password,
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: 'Error registering user',
+      error,
+    });
+  }
 };
 
 export const renewToken = (req, res = response) => {
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -9,14 +9,56 @@ import {
   loginUser,
   renewToken,
 } from '../controllers/auth.controllers';
+import { check } from 'express-validator';
 
 const router = Router();
 
 // Login Route
-router.route('/').post(loginUser);
+router
+  .route('/')
+  .post(
+    [
+      check('email')
+        .notEmpty()
+        .withMessage('email is required')
+        .isEmail()
+        .withMessage('email must be valid')
+        .isLength({ min: 2, max: 50 })
+        .withMessage('email must be between 2 and 50 characters'),
+      check('password')
+        .notEmpty()
+        .withMessage('password is required')
+        .isLength({ min: 8, max: 12 })
+        .withMessage('password must be between 8 and 12 characters'),
+    ],
+    loginUser
+  );
 
 // Register Route
-router.route('/new').post(createUser);
+router
+  .route('/new')
+  .post(
+    [
+      check('name')
+        .notEmpty()
+        .withMessage('name is required')
+        .isLength({ min: 2, max: 10 })
+        .withMessage('name must be between 2 and 10 characters'),
+      check('email')
+        .notEmpty()
+        .withMessage('email is required')
+        .isEmail()
+        .withMessage('email must be valid')
+        .isLength({ min: 2, max: 50 })
+        .withMessage('email must be between 2 and 50 characters'),
+      check('password')
+        .notEmpty()
+        .withMessage('password is required')
+        .isLength({ min: 8, max: 12 })
+        .withMessage('password must be between 8 and 12 characters'),
+    ],
+    createUser
+  );
 
 // Renew Token Route
 router.route('/renew').get(renewToken);
